ce08ad089ce9f09496a9ba49392215e4c1680504
宋辉 <hsong@wisu.com.cn>
2020-08-18 18:41:34 +0800

    配置环境变量

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 App({
   globalData: {
     userInfo: wx.getStorageSync('userInfo'),
-    hostUrl: 'https://api.zjjcjc888.com',
+    hostUrl: '',
     // hostUrl: 'http://116.62.168.148:8102',
     header: {
       "Content-Type": 'application/json',
@@ -11,38 +11,19 @@ App({
     mapLocation: wx.getStorageSync('mapLocation')
   },
 	onLaunch: function () {
-    if (!this.globalData.userInfo) {
-      return wx.navigateTo({
-        url: '/pages/authorization/authorization'
-      })
-    }
-    return false;
-	// 登录
-	wx.login({
-		success: res => {
-		// 发送 res.code 到后台换取 openId, sessionKey, unionId
+		let host = '';
+		let NODE_ENV = 'pro';
+		const fileManager = wx.getFileSystemManager();
+		try{
+			fileManager.accessSync('/local.txt');
+			NODE_ENV = 'dev';
+		}catch(e){}
+		if( NODE_ENV === 'pro' ){
+			host = 'https://api.cloudsymbol.cn';
+		}else{
+			host = 'https://api.zjjcjc888.com';
 		}
-	})
-	// 获取用户信息
-	wx.getSetting({
-		success: res => {
-		if (res.authSetting['scope.userInfo']) {
-			// 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-			wx.getUserInfo({
-			success: res => {
-				// 可以将 res 发送给后台解码出 unionId
-				this.globalData.userInfo = res.userInfo
-
-				// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-				// 所以此处加入 callback 以防止这种情况
-				if (this.userInfoReadyCallback) {
-				this.userInfoReadyCallback(res)
-				}
-			}
-			})
-		}
-		}
-	})
+		this.globalData.hostUrl = host
 	},
   	/**
 	 * 封封微信的的request
